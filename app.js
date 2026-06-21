d8b2ce2808bc71a5009a64d03720e6356b440fa6
Avonda Fogan <afogan@gmail.com>
2026-06-21 13:20:02 -0400

    added user & token language

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ import morgan from "morgan";
 
 import tracksRouter from "#api/tracks";
 import playlistsRouter from "#api/playlists";
+import getUserFromToken from "#middleware/getUserFromToken";
+import playlistsRouter from "#api/playlists";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(getUserFromToken);
+
 app.use("/tracks", tracksRouter);
 app.use("/playlists", playlistsRouter);
+app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
   // A switch statement can be used instead of if statements
