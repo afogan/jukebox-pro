ee051ebc8076277dc7d923ada9d90deef2998b72
Avonda Fogan <afogan@gmail.com>
2026-06-21 12:52:17 -0400

    updated tracks.js with refactored router code

--- api/tracks.js
+++ api/tracks.js
@@ -3,14 +3,26 @@ const router = express.Router();
 export default router;
 
 import { getTracks, getTrackById } from "#db/queries/tracks";
+import { getPlaylistByTrackId } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
 
 router.get("/", async (req, res) => {
   const tracks = await getTracks();
   res.send(tracks);
 });
 
-router.get("/:id", async (req, res) => {
-  const track = await getTrackById(req.params.id);
-  if (!track) return res.status(404).send("Track not found.");
-  res.send(track);
+router.param("id", async (req, res, next, id) => {
+const track = await getTrackById(id);
+if (!track) return res.status(404).send("Track not found.");
+req.track = track;
+next();
+});
+
+router.get("/:id", (req, res) => {
+  res.send(req.track);
+});
+ 
+router.get("/:id/palylists", requireUser, async (req, res) => {
+  const playlists = await getPlaylistByTrackId(req.track.id);
+  res.send(playlists);
 });
